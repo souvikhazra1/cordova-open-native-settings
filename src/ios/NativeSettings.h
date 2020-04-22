6748c1d85df0ad2b317c705e993e03a3c776e6d4
Souvik Hazra <souvik.babanhazra@gmail.com>
2020-04-22 16:30:20 +0530

    iOS will reject app if prefs or App-Prefs used, only opening settings have to live with that

--- src/ios/NativeSettings.h
+++ src/ios/NativeSettings.h
@@ -1,9 +1,8 @@
 #import <Cordova/CDVPlugin.h>
-#import <Cordova/CDVInvokedUrlCommand.h>
 
 @interface NativeSettings : CDVPlugin
 
 #define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
 
-- (void)open:(CDVInvokedUrlCommand*)command;
+- (void)open:()command;
 @end
--- src/ios/NativeSettings.m
+++ src/ios/NativeSettings.m
@@ -2,8 +2,8 @@
 
 @implementation NativeSettings
 
-- (BOOL)do_open:(NSString *)pref {
-	if ([[UIApplication sharedApplication] openURL:[NSURL URLWithString:pref]]) {
+- (BOOL)do_open:()pref {
+	if ([[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString]]) {
 		return YES;
 	} else {
 		return NO;
@@ -13,13 +13,18 @@
 - (void)open:(CDVInvokedUrlCommand*)command
 {
 	CDVPluginResult* pluginResult = nil;
-	NSString* key = [command.arguments objectAtIndex:0];
-	NSString* prefix = @"prefs:";
-	BOOL result = NO;
+	BOOL result = [self do_open];
 
-	/*if(SYSTEM_VERSION_LESS_THAN(@"11.3")){
+
+	// iOS will reject app if prefs or App-Prefs used, only opening settings have to live with that
+
+
+	/*NSString* key = [command.arguments objectAtIndex:0];
+	NSString* prefix = @"App-Prefs:";
+
+	if(SYSTEM_VERSION_LESS_THAN(@"11.3")){
         prefix = @"app-settings:";
-    }*/
+    }
 
 	
     if ([key isEqualToString:@"application_details"]) {
@@ -186,7 +191,7 @@
 	}
 	else {
 		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Invalid Action"];
-	}
+	}*/
 		
 	if (result) {
 		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Opened"];
